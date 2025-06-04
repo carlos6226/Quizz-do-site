f3ab7386ff122b162991f89607be463a833ed02b
carlos6226 <145015241+carlos6226@users.noreply.github.com>
2025-06-04 12:31:45 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    question: "Nice to cu trabalha com o que?",
+    answers: [
+      { option: "Aneis,alianças,pulseras entre outros", correct: true },
+      { option: "Carros", correct: false },
+      { option: "Bundas", correct: false },
+    ],
+  },
+
+  {
+    question: "Quais as cores da empresa?",
+    answers: [
+      { option: "verde claro e roxo", correct: true },
+      { option: "verde e roxo escuro", correct: false },
+      { option: "verde escuro e roxo", correct: false },
+    ],
+  },
+
+  {
+    question: "Quanto faturamos mês passado?",
+    answers: [
+      { option: "$77,00", correct: true },
+      { option: "$2000,00", correct: false },
+      { option: "4300,00", correct: false },
+    ],
+  },
+
+{
+    question: "Vale mesmo se associar a nós?",
+    answers: [
+      { option: "Obvio", correct: true },
+      { option: "Sim", correct: false },
+      { option: "Claro", correct: false },
+    ],
+  },
+]
+
+
